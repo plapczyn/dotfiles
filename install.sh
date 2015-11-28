17f54120b16429fb57f81cb497ac338386ff6bf3
Philip Lapczynski <philip.lapczynski@gmail.com>
2015-11-28 09:23:57 -0500

    added setup for git

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
+# Set up Git
+git config --global user.name "Philip Lapczynski"
+git config --global user.email "philip.lapczynski@gmail.com"
+
+# Set up symlinks
 ln -s ~/.dotfiles/vim/ ~/.vim
 ln -s ~/.dotfiles/_gvimrc ~/.vimrc
+
